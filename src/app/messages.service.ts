bb3857043dcade332fa91aaad9acc568a2fdf59c
Otávio <otavioalmeida650@gmail.com>
2019-03-16 16:52:27 -0300

    needs to finish HTTP step

---
+++ src/app/messages.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+
+  constructor() { }
+
+    msgs: string[] = [];
+
+    add(msg: string) {
+        this.msgs.push(msg);
+    }
+
+    clear() {
+        this.msgs = [];
+    }
+
+}
